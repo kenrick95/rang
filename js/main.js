429551708f6fdc2172ec1109c6daa677a3da1d72
Kenrick <kenrick95@gmail.com>
2020-06-10 22:52:03 +0800

    Refactor: separate data and render logic

--- js/main.js
+++ js/main.js
@@ -1,226 +1,295 @@
+'use strict';
+const sharedFetchParams = {
+  action: 'query',
+  prop: 'imageinfo|revisions',
+  format: 'json',
+  rvprop: 'timestamp|user',
+  generator: 'categorymembers',
+  gcmprop: 'title|type|timestamp',
+  gcmnamespace: '6',
+  gcmlimit: 500,
+  gcmsort: 'timestamp',
+};
+const rangConfig = {
+  url: 'https://id.wikipedia.org',
+};
+const today = new Date();
+const lastWeek = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate() - 7,
+  today.getHours(),
+  today.getMinutes(),
+  today.getSeconds(),
+  today.getMilliseconds()
+);
+
 $(document).ready(function () {
-  var rangConfig = {
-    url: '//id.wikipedia.org',
+  /**
+   * [[Kategori:Artikel yang layak untuk dihapus]]
+   * [[Kategori:Halaman yang telah jatuh tempo penghapusan]]
+   */
+  let dataStore = {};
+  let imageUsageStore = {};
+  let imageContentStore = {};
+  let imageThumbStore = {};
+  let pageState = {
+    selectedPageId: null,
     reason: '[[WP:KPC#B5]]',
   };
-  var rangData = null;
-  var today = new Date();
-  var lastWeek = new Date(
-    today.getFullYear(),
-    today.getMonth(),
-    today.getDate() - 7,
-    today.getHours(),
-    today.getMinutes(),
-    today.getSeconds(),
-    today.getMilliseconds()
-  );
 
-  $.ajax({
-    url: rangConfig.url + '/w/api.php',
-    dataType: 'jsonp',
-    data: {
-      action: 'query',
-      prop: 'imageinfo|revisions',
-      format: 'json',
-      rvprop: 'timestamp|user',
-      generator: 'categorymembers',
-      gcmtitle: 'Kategori:Artikel yang layak untuk dihapus',
-      gcmprop: 'title|type|timestamp',
-      gcmnamespace: '6',
-      gcmlimit: 500,
-      gcmsort: 'timestamp',
-      gcmend: lastWeek.toISOString(),
-    },
-  })
-    .done(function (data) {
-      if (!data.query) {
-        console.warn('Either no data or error', data);
-        return;
-      }
-      rangData = data.query.pages;
-      var coll = $('<div/>');
-      for (var x in rangData) {
-        if (rangData.hasOwnProperty(x)) {
-          $(coll).append(
-            $('<li/>', {
-              html: $('<a/>', {
-                class: 'rang-item',
-                id: 'rang-item-' + x,
-                href: '#',
-                'data-pageid': x,
-                text: rangData[x].title,
-              }),
-            })
-          );
-        }
-      }
-      $('#rang-nav > ul').append(coll.children());
-      $('.rang-item').on('click', function () {
-        $('#rang-nav .active').removeClass('active');
-        $(this).parent().addClass('active');
-        var pageid = $(this).data('pageid');
-        var data = rangData[pageid];
-        var coll = $('<div/>')
-          .append(
+  fetchData();
+  function render() {
+    if (pageState.selectedPageId) {
+      fetchImageInfo(pageState.selectedPageId);
+    }
+    renderInitial();
+    renderMainView();
+  }
+  async function fetchData() {
+    await Promise.all([fetchAfdState(), fetchAfdDuedState()]);
+    const firstDatum = Object.values(dataStore);
+    pageState.selectedPageId = firstDatum[0] ? firstDatum[0].pageid : null;
+    render();
+  }
+  async function fetchAfdState() {
+    const response = await $.ajax({
+      url: rangConfig.url + '/w/api.php',
+      dataType: 'jsonp',
+      data: {
+        ...sharedFetchParams,
+        gcmtitle: 'Kategori:Artikel yang layak untuk dihapus',
+        gcmend: lastWeek.toISOString(),
+      },
+    });
+
+    if (!response.query) {
+      console.warn('[fetchAfdState] No data', response);
+      return;
+    }
+    dataStore = { ...dataStore, ...response.query.pages };
+  }
+  async function fetchAfdDuedState() {
+    const response = await $.ajax({
+      url: rangConfig.url + '/w/api.php',
+      dataType: 'jsonp',
+      data: {
+        ...sharedFetchParams,
+        gcmtitle: 'Kategori:Halaman yang telah jatuh tempo penghapusan',
+        gcmend: new Date().toISOString(),
+      },
+    });
+    if (!response.query) {
+      console.warn('[fetchAfdDuedState] No data', response);
+      return;
+    }
+    dataStore = { ...dataStore, ...response.query.pages };
+  }
+  async function fetchImageInfo(pageid) {
+    if (!pageid) {
+      return;
+    }
+    if (imageUsageStore[pageid]) {
+      // Data already fetched, do early return to avoid infinite recursion
+      return;
+    }
+    const title = dataStore[pageid].title;
+    const response = await $.ajax({
+      url: rangConfig.url + '/w/api.php',
+      dataType: 'jsonp',
+      data: {
+        action: 'query',
+        prop: 'revisions|imageinfo',
+        list: 'imageusage',
+        iiprop: 'url',
+        iiurlwidth: 220,
+        format: 'json',
+        rvprop: 'content',
+        rvparse: 1,
+        pageids: pageid,
+        iulimit: 500,
+        iutitle: title,
+      },
+    });
+    if (!response.query) {
+      console.warn('[fetchImageInfo] No data', pageid, response);
+      return;
+    }
+    imageUsageStore = {
+      ...imageUsageStore,
+      [pageid]: response.query.imageusage,
+    };
+
+    const html = response.query.pages[pageid].revisions[0]['*'].replace(
+      /href="\/w/g,
+      'href="' + rangConfig.url + '/w'
+    );
+    imageContentStore = {
+      ...imageContentStore,
+      [pageid]: html,
+    };
+
+    imageThumbStore = {
+      ...imageThumbStore,
+      [pageid]: response.query.pages[pageid].imageinfo[0].thumburl,
+    };
+
+    render();
+  }
+
+  function handleItemClicked() {
+    let pageid = $(this).data('pageid');
+    pageState.selectedPageId = pageid;
+    render();
+  }
+
+  function renderInitial() {
+    const children = [];
+    for (const item of Object.values(dataStore)) {
+      children.push(
+        $('<li/>', {
+          html: $('<a/>', {
+            class: 'rang-item',
+            id: 'rang-item-' + item.pageid,
+            href: '#',
+            'data-pageid': item.pageid,
+            text: item.title,
+          }),
+        })
+      );
+    }
+    $('#rang-nav > ul').html(children);
+
+    $('.rang-item').on('click', handleItemClicked);
+  }
+
+  function renderMainView() {
+    const pageid = pageState.selectedPageId;
+    if (!pageid) {
+      return;
+    }
+
+    const data = dataStore[pageid];
+    if (!data) {
+      return;
+    }
+    const imageUsages = imageUsageStore[pageid]
+      ? Object.values(imageUsageStore[pageid])
+      : [];
+      
+    $('#rang-main').html(
+      $('<div/>')
+        .append(
+          $('<div/>', {
+            id: 'rang-right',
+            html: $('<img />', {
+              id: 'rang-thumb',
+              width: 220,
+              src: imageThumbStore[pageid] ? imageThumbStore[pageid] : null,
+            }),
+          }).append(
             $('<div/>', {
-              id: 'rang-right',
-              html: $('<img/>', {
-                id: 'rang-thumb',
-                width: 220,
-              }),
+              id: 'rang-iu',
             }).append(
-              $('<div/>', {
-                id: 'rang-iu',
-              })
+              $('<ul />').html(
+                imageUsages.map((imageUsage) => {
+                  return $('<li/>', {
+                    html: $('<a/>', {
+                      href: rangConfig.url + '/wiki/' + imageUsage.title,
+                      text: imageUsage.title,
+                    }),
+                  });
+                })
+              )
             )
           )
-          .append(
-            $('<h1/>', {
-              html: $('<a/>', {
-                href: rangConfig.url + '/wiki/' + data.title,
-                text: data.title,
-                target: '_blank',
-              }),
-            })
-          )
-          .append('<br>')
-          .append(
-            $('<input/>', {
-              id: 'rang-reason',
-              type: 'text',
-              class: 'form-control',
-              placeholder: 'Alasan penghapusan',
-              value: rangConfig.reason,
-            })
-          )
-          .append(
-            $('<button/>', {
-              class: 'btn btn-danger',
-              text: 'Hapus',
-              id: 'rang-delete',
-              'data-pageid': pageid,
-            })
-          )
-          .append(
-            $('<div/>', {
-              id: 'rang-content',
-              text: 'Memuat ...',
-            })
-          );
-        $('#rang-status-img-timestamp').text(data.imageinfo[0].timestamp);
-        $('#rang-status-img-user').html(
-          $('<a/>', {
-            href: rangConfig.url + '/wiki/User:' + data.imageinfo[0].user,
-            text: data.imageinfo[0].user,
+        )
+        .append(
+          $('<h1/>', {
+            html: $('<a/>', {
+              href: rangConfig.url + '/wiki/' + data.title,
+              text: data.title,
+              target: '_blank',
+            }),
           })
-        );
-        $('#rang-status-rev-timestamp').text(data.revisions[0].timestamp);
-        $('#rang-status-rev-user').html(
-          $('<a/>', {
-            href: rangConfig.url + '/wiki/User:' + data.revisions[0].user,
-            text: data.revisions[0].user,
+        )
+        .append('<br />')
+        .append(
+          $('<input/>', {
+            id: 'rang-reason',
+            type: 'text',
+            class: 'form-control',
+            placeholder: 'Alasan penghapusan',
+            value: pageState.reason,
+          }).on('input', function () {
+            pageState.reason = $(this).val();
           })
-        );
-
-        $.ajax({
-          url: rangConfig.url + '/w/api.php',
-          dataType: 'jsonp',
-          data: {
-            action: 'query',
-            prop: 'revisions|imageinfo',
-            list: 'imageusage',
-            iiprop: 'url',
-            iiurlwidth: 220,
-            format: 'json',
-            rvprop: 'content',
-            rvparse: 1,
-            pageids: pageid,
-            iulimit: 500,
-            iutitle: data.title,
-          },
-        }).done(function (data) {
-          var iu = data.query.imageusage;
-
-          if (iu.length > 0) {
-            var collIu = $('<ul/>');
-            for (var iuKey in iu) {
-              if (iu.hasOwnProperty(iuKey)) {
-                collIu.append(
-                  $('<li/>', {
-                    html: $('<a/>', {
-                      href: rangConfig.url + '/wiki/' + iu[iuKey].title,
-                      text: iu[iuKey].title,
-                    }),
-                  })
-                );
-              }
-            }
-
-            $('#rang-iu').append(collIu);
-          }
-
-          var html = data.query.pages[pageid].revisions[0]['*'];
-          html = html.replace(/href="\/w/g, 'href="' + rangConfig.url + '/w');
-          $('#rang-content').html(html);
-          $('#rang-thumb').attr(
-            'src',
-            data.query.pages[pageid].imageinfo[0].thumburl
-          );
-        });
-
-        $('#rang-main').html(coll);
-
-        $('#rang-reason').on('input', function () {
-          rangConfig.reason = $(this).val();
-        });
-        if (
-          $('#rang-username').length === 0 ||
-          $('#rang-username').data('can-delete') !== 1
-        ) {
-          $('#rang-delete').attr('disabled', 'disabled');
-        }
-
-        $('#rang-delete').on('click', function () {
-          var pageid = $(this).data('pageid'),
-            reason = rangConfig.reason;
-          $(this).attr('disabled', 'disabled');
-          $.ajax({
-            url: 'handler.php',
-            dataType: 'json',
-            data: {
-              func: 'delete_image',
-              x1: pageid,
-              x2: reason,
-            },
-          }).done(function (data) {
-            $('#rang-delete').removeAttr('disabled');
-            if (!!data.delete) {
-              //$("#rang-item-" + pageid).parent().next().children().click();
-              $('#rang-item-' + pageid)
+        )
+        .append(
+          $('<button />', {
+            class: 'btn btn-danger',
+            text: 'Hapus',
+            id: 'rang-delete',
+            'data-pageid': pageid,
+          }).on('click', async function () {
+            const reason = pageState.reason;
+            $(this).attr('disabled', 'disabled');
+            const response = await $.ajax({
+              url: 'handler.php',
+              dataType: 'json',
+              data: {
+                func: 'delete_image',
+                x1: pageid,
+                x2: reason,
+              },
+            });
+            $(this).removeAttr('disabled');
+            if (!!response.delete) {
+              delete dataStore[pageid];
+              const nextPageid = $('#rang-item-' + pageid)
                 .parent()
-                .remove();
+                .next()
+                .children()
+                .data('pageid');
+              if (nextPageid) {
+                pageState.selectedPageId = nextPageid;
+              }
+
+              render();
             } else {
               alert('Gagal menghapus berkas dengan id ' + pageid);
-              $('#rang-item-' + pageid).click();
             }
-          });
-          $('#rang-item-' + pageid)
-            .parent()
-            .next()
-            .children()
-            .click();
-        });
-      });
-    })
-    .done(function () {
-      $('.rang-item').first().click();
-    });
-  function resizeHandler() {
-    var newHeight = $(window).innerHeight() - 65;
-
-    $('#rang-main').height(newHeight);
-    $('#rang-nav').height(newHeight);
+          })
+        )
+        .append(
+          $('<div />', {
+            id: 'rang-content',
+            text: 'Memuat ...',
+          })
+        )
+    );
+    $('#rang-status-img-timestamp').text(data.imageinfo[0].timestamp);
+    $('#rang-status-img-user').html(
+      $('<a/>', {
+        href: rangConfig.url + '/wiki/User:' + data.imageinfo[0].user,
+        text: data.imageinfo[0].user,
+      })
+    );
+    $('#rang-status-rev-timestamp').text(data.revisions[0].timestamp);
+    $('#rang-status-rev-user').html(
+      $('<a/>', {
+        href: rangConfig.url + '/wiki/User:' + data.revisions[0].user,
+        text: data.revisions[0].user,
+      })
+    );
+    if (
+      $('#rang-username').length === 0 ||
+      $('#rang-username').data('can-delete') !== 1
+    ) {
+      $('#rang-delete').attr('disabled', 'disabled');
+    }
+    if (imageContentStore[pageid]) {
+      $('#rang-content').html(imageContentStore[pageid]);
+    }
   }
-  $(window).resize(resizeHandler);
-  resizeHandler();
 });
